71236492f95542b3c33919051b62ff171c77220e
user.name : yashodip wani <yashodipwani172004@gmail.com>
2026-07-18 22:55:12 +0530

    Fun Question
    Fun Declaration question Solved

---
+++ Functions/question.js
@@ -0,0 +1,64 @@
+//1 Create a function that prints "Hello World".
+function abcd() {
+    console.log("Hello World")
+}
+abcd()
+
+//Write a function to add two numbers.
+
+// function greed(a, b) {
+//     return a + b
+// }
+// console.log(greed(10, 20))
+
+
+//3. Write a function to subtract two numbers.
+// function sub(a, b) {
+//     return a / b
+// }
+// console.log(sub(10, 5))
+
+//4. Write a function to multiply two numbers.
+//5. Write a function to divide two numbers.
+
+//Write a function to calculate the squareand cubs  of a number.
+// function squ(nums) {
+//     return nums * nums * nums
+
+// }
+// console.log(squ(10));
+
+console.dir("Yashodip");
+
+
+// Write a function to print numbers from 1 to 10.
+// function print(){
+//     for(let i= 1; i<=10;i++){
+//         console.log(i);
+//     }
+// }
+// print()
+
+
+//Write a function to find the largest of two numbers.
+
+// function larg(a , b ){
+//     if(a> b){
+//        return a
+//     }
+//     else {
+//       return b;
+//     }
+// }
+// console.log(larg(10 , 20));
+
+// Write a function to check if a number is even or odd.
+
+// function EvenOdd(nums) {
+//     return nums % 2 === 0 ? "Even" : "Odd";
+// }
+// console.log(EvenOdd(11));
+
+
+
+//! @Function Expression 
