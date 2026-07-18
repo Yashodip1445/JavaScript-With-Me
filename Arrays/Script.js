a094a693aa14acf1ecb63991e833ca43ac7e8f7a
user.name : yashodip wani <user.email : yashodipwani172004@gmail.com>
2026-07-18 16:48:14 +0530

    Arrays Method

--- Arrays/Script.js
+++ Arrays/Script.js
@@ -1,2 +1,20 @@
-alert("Arrays")
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+console.log(array)
+
+// push  
+// array.push(11)
+
+//2. pop
+
+// array.pop()
+// console.log(array)
+
+//3. unshift
+
+// array.unshift(33)
+// console.log(array)
+
+// shift
+// array.shift()
+// console.log(array)
