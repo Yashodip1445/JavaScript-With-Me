b0724d893d2ab1b2dcbe05aa16efb832e7dbc7db
user.name : yashodip wani <yashodipwani172004@gmail.com>
2026-07-22 10:33:57 +0530

    THE DOM

---
+++ The DOM/Script.js
@@ -0,0 +1,21 @@
+ 
+ // The DOM 
+
+ /* 
+1. getElementsbyId
+2. fetElementsByclassName 
+3. querySelector
+4. querySelctorAll
+ */
+ 
+ let chacha = document.querySelector("h1")
+ //h1.innerHTML= "<i>The Dom !   </i>"
+// h1.textContent = " hello ji"
+//h1.innerText
+
+console.dir(chacha)
+
+
+
+ 
+  
