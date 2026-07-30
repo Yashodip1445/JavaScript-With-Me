c98e34ec9ad240e30132297ef2500e62354e839f
user.name : yashodip wani <yashodipwani172004@gmail.com>
2026-07-30 16:37:14 +0530

    EventListerner

---
+++ Event And Event Handling/script.js
@@ -0,0 +1,13 @@
+
+
+let h1= document.querySelector("h1");
+h1.addEventListener("click" , function(){
+h1.style.color= "green"
+})
+
+let inp = document.querySelector("input");
+
+inp.addEventListener("input" , function(dets){
+    console.log(dets)
+})
+
